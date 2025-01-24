895ffa09dae3c2b6963efab4f6293277f763d2d6
Jeferson Silva <jefersonrobertors3002@outlook.com>
2025-01-24 18:47:09 -0300

    Update Random.php

--- src/Random.php
+++ src/Random.php
@@ -9,6 +9,7 @@ final class Random {
             throw new \InvalidArgumentException("Lenght must be greater than 0.");
         }
         $salt = implode($salts);
+        
         $entropy = hash('sha256', uniqid('', true) . microtime() . $salt . random_bytes(16));
         $characterPool = preg_replace('/[^a-zA-Z0-9]/u', '', base64_encode($entropy));
 
@@ -17,7 +18,7 @@ final class Random {
         if($poolLength <= 0) {
             throw new \RuntimeException("Character pool is empty. Ensure enough entropy is provided.");
         }
-       $characters = [];
+        $characters = [];
         
         for($i = 0; $i < $length; $i++) {
             $characters[] = $characterPool[random_int(0, $poolLength - 1)];
